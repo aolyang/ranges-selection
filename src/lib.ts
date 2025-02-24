1be0c05d3a464fb78a4a7318fe4840bf0a239e5c
aolyang <aolyang@foxmail.com>
2025-02-24 18:52:44 +0800

    feat: merge function

--- src/lib.ts
+++ src/lib.ts
@@ -1,20 +1,6 @@
 type Range = [start: number, end: number]
 
 /**
- * Merges ranges stored in flat array format
- * @param exists - Existing ranges in flat array format (e.g., [1,3,5,6])
- * @param ranges - New ranges to merge in flat array format (e.g., [5,7,9,10])
- * @returns Merged ranges in flat array format (e.g., [1,3,5,7,9,10])
- * @example
- * [1, 3, 5, 6] + [5, 7, 9, 10] = [1, 3, 5, 7, 9, 10]
- * [1, 3, 5, 7, 9, 10] (in meaning) equal to [[1, 3], [5, 7], [9, 10]]
- * */
-function merge(exists: number[], ranges: number[]): number[] {
-
-    return exists
-}
-
-/**
  * Merges arrays of range tuples
  * @param exists - Existing array of range tuples (e.g., [[1,3], [5,6]])
  * @param ranges - New array of range tuples to merge (e.g., [[5,7], [9,10]])
@@ -22,14 +8,35 @@ function merge(exists: number[], ranges: number[]): number[] {
  * @example
  * [[1, 3], [5, 6]] + [[5, 7], [9, 10]] = [[1, 3], [5, 7], [9, 10]]
  * */
-function mergeArrays(exists: Range[], ranges: Range[]): Range[] {
+function merge(exists: Range[], ranges: Range[]): Range[] {
+    const _ranges = exists.concat(ranges).sort((a, b) => a[0] - b[0])
 
-    return exists
+    const merged: Range[] = []
+
+    let current: Range
+    while (_ranges.length) {
+        current = _ranges.shift() as Range
+
+        if (current.length !== 2) return []
+        if (merged.length) {
+            const last = merged[merged.length - 1]
+            if (current[0] <= last[1]) {
+                last[1] = Math.max(last[1], current[1])
+                continue
+            } else if (current[0] === last[1] + 1) {
+                last[1] = current[1]
+                continue
+            }
+        }
+
+        merged.push(current)
+    }
+
+    return merged
 }
 
 export {
-    merge,
-    mergeArrays
+    merge
 }
 
 if (import.meta.vitest) {
@@ -41,36 +48,21 @@ if (import.meta.vitest) {
 
     describe("`merge` functions", () => {
         let message: string
-        let specs: FlatSpec[]
-
-        message = "should handle empty specs, merge $exists with $ranges, get $expected"
-        specs = [
-            { exists: [], ranges: [], expected: [] },
-            { exists: [1, 3], ranges: [], expected: [1, 3] },
-            { exists: [], ranges: [1, 3], expected: [1, 3] }
-        ]
-
-        it.each(specs)(message, ({ exists, ranges, expected }) => {
-            expect(merge(exists, ranges)).toEqual(expected)
-        })
-
-        message = "should merge ranges, merge $exists with $ranges, get $expected"
-        specs = [
-            { exists: [1, 3], ranges: [5, 7], expected: [1, 3, 5, 7] },
-            { exists: [1, 3], ranges: [4, 7, 9, 10], expected: [1, 7, 9, 10] },
-            { exists: [1, 3, 5, 7, 10, 14], ranges: [3, 5], expected: [1, 7, 10, 14] },
-            { exists: [1, 3, 5, 7, 10, 14], ranges: [3, 5, 7, 10], expected: [1, 14] }
-        ]
-        it.each(specs)(message, ({ exists, ranges, expected }) => {
-            expect(merge(exists, ranges)).toEqual(expected)
-        })
-    })
-
-    describe("`mergeArrays` functions", () => {
-        let message: string
         let specs: TupleSpec[]
 
-        message = "should handle empty specs, merge $exists with $ranges, get $expected"
+        const illegalRange = [1] as any
+        message = "when items length not valid, return empty range []"
+        specs = [
+            { exists: [illegalRange], ranges: [], expected: [] },
+            { exists: [], ranges: [illegalRange], expected: [] },
+            { exists: [[1, 3], illegalRange], ranges: [], expected: [] },
+            { exists: [], ranges: [[1, 3], illegalRange], expected: [] }
+        ]
+        it.each(specs)(message, ({ exists, ranges, expected }) => {
+            expect(merge(exists, ranges)).toEqual(expected)
+        })
+
+        message = "should handle empty specs, merge $exists with $ranges"
         specs = [
             { exists: [], ranges: [], expected: [] },
             { exists: [[1, 3]], ranges: [], expected: [[1, 3]] },
@@ -78,17 +70,17 @@ if (import.meta.vitest) {
         ]
 
         it.each(specs)(message, ({ exists, ranges, expected }) => {
-            expect(mergeArrays(exists, ranges)).toEqual(expected)
+            expect(merge(exists, ranges)).toEqual(expected)
         })
-        message = "should merge ranges, merge $exists with $ranges, get $expected"
+        message = "should merge ranges, merge $exists with $ranges"
         specs = [
             { exists: [[1, 3]], ranges: [[5, 7]], expected: [[1, 3], [5, 7]] },
             { exists: [[1, 3]], ranges: [[4, 7], [9, 10]], expected: [[1, 7], [9, 10]] },
             { exists: [[1, 3], [5, 7], [10, 14]], ranges: [[3, 5]], expected: [[1, 7], [10, 14]] },
-            { exists: [[1, 3], [5, 7], [10, 14]], ranges: [[3, 5], [7, 10]], expected: [[1, 7], [10, 14]] }
+            { exists: [[1, 3], [5, 7], [10, 14]], ranges: [[3, 5], [7, 10]], expected: [[1, 14]] }
         ]
         it.each(specs)(message, ({ exists, ranges, expected }) => {
-            expect(mergeArrays(exists, ranges)).toEqual(expected)
+            expect(merge(exists, ranges)).toEqual(expected)
         })
     })
 }
